00e42af5d1df664a6fb3ce8b9e6318a45fd6b2af
changsubchang <charlie9007@naver.com>
2025-02-23 17:03:11 +0900

    samples added in config

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 answer_examples = [
-    { },
+    {
+        "input": "How many of the years have Glacier lost its mass in continuous years?", 
+        "answer": """According to page S23, the Galcier has been losing it mass for 36 years in a row
+"""
+    },
 ]
